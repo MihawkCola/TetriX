47dc077679e96739d38cce8acd06d7c0b4dcba6b
MihawkCola <marcel.sauer1997@gmail.com>
2020-05-07 15:39:59 +0200

    first commit

---
+++ app/src/main/java/de/prog3/tetrix/MainActivity.java
@@ -0,0 +1,14 @@
+package de.prog3.tetrix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
